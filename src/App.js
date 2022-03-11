1af8dd372c31823ba8c25ae6d8091c14000f3e94
anubhav100rao <anubhav100rao@gmail.com>
2022-03-11 12:33:13 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,90 +1,63 @@
-import React from 'react';
-import './App.css';
-import { useState, useReducer, useEffect } from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import MovieCard from './MovieCard'
+import SearchIcon from './search.svg'
 
-function ShowInput() {
-  const [inputState, setInputState] = useState("Hi!")
-  const changeInputState = (event) => {
-    setInputState(event.target.value)
-  }
-  return <div>
-    <input placeholder='enter something...' onChange={changeInputState}/>
-    {inputState}
-  </div>
-}
+const API_URL = "http://www.omdbapi.com?apikey=352463f"
 
 
-function Counter() {
-  const [counter, setCounter] = useState(0);
-  const increment = () => {
-    setCounter(counter+1)
-  }
-  return <div>
-    <h1>Hello React</h1>
-      {counter} <button onClick={ increment } >increment</button>
-      <br/>
-  </div>
-}
 
-function ReducerExample() {
-  function reducer(state, action) {
-    switch(action.type) {
-      case 'increment':
-        return {count: state.count+1, isVisible: state.isVisible}
-      case 'toggleVisibility':
-        return {count: state.count, isVisible: !state.isVisible}
-      default:
-        return state
+const App = () => {
+
+    const [movies, setMovies] =  useState([])
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+        setMovies(data.Search)
     }
-  }
-  const [state, dispatch] = useReducer(reducer, {count: 0, isVisible: true})
-  return <div>
-    {state.count}
-    <button
-      onClick={() => {
-          dispatch({type: 'increment'})
-          dispatch({type: 'toggleVisibility'})
-        }
-      }
-    >
-      Increment and toggle
-    </button>
-    {state.isVisible && <p>Show text current state's square value is {state.count * state.count}</p>}
-  </div>
+
+    useEffect(() => {
+        searchMovies("Spiderman")
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>MovieLand</h1>
+            <div className='search'>
+                <input 
+                    placeholder='search for movies'
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="searchicon" 
+                    onClick={() => {
+                        searchMovies(searchTerm)
+                    }}
+                />
+            </div>
+            {
+                movies.length > 0 ? (
+                    <div className='container'>
+                        {
+                            movies.map(movie => (
+                                <MovieCard movie={movie}/>
+                            ))
+                        }
+                    </div>
+                )
+                : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
+            
+        </div>
+    )
 }
 
-function EffectExample() {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    console.log("useEffect callBack called")
-    axios.get("https://jsonplaceholder.typicode.com/comments")
-      .then((res) => {
-        setData(res.data)
-      })
-  }, [])
-  return <div>
-    <h3>useEffect hook</h3>
-    <ul>
-      {
-        data.map(item => <div>{item.email} ******** {item.name}<br/><br/></div>)
-      }
-    </ul>
-  </div>
-}
-
-
-function App() {
-  
-  return (
-    <div className='App'>
-      <Counter/>
-      <ShowInput/>
-      <br/>
-      <ReducerExample/>
-      <EffectExample/>
-    </div>
-  );
-}
-
-export default App;
+export default App
